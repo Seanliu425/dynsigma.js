a178f77cb14a2f0a30e0d14e530862e02f8feae2
Sean Liu <skxliu@Seans-MacBook-Air-3.local>
2024-10-16 17:47:52 -0500

    added health community zones

--- demo/src/views/CommunitiesPanel.tsx
+++ demo/src/views/CommunitiesPanel.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useMemo, useState } from "react";
 import { useSigma } from "react-sigma-v2";
 import { MdCategory } from "react-icons/md";
-import { keyBy, mapValues, sortBy, values } from "lodash";
+import { keyBy, mapValues, sortBy, values, groupBy } from "lodash";
 import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 
 import { Community, FiltersState } from "../types";
@@ -27,9 +27,6 @@ const CommunitiesPanel: FC<{
 
   const [visibleNodesPerCommunity, setVisibleNodesPerCommunity] = useState<Record<string, number>>(nodesPerCommunity);
   useEffect(() => {
-    // To ensure the graphology instance has up to data "hidden" values for
-    // nodes, we wait for next frame before reindexing. This won't matter in the
-    // UX, because of the visible nodes bar width transition.
     requestAnimationFrame(() => {
       const index: Record<string, number> = {};
       graph.forEachNode((_, { community, hidden }) => !hidden && (index[community] = (index[community] || 0) + 1));
@@ -41,6 +38,65 @@ const CommunitiesPanel: FC<{
     () => sortBy(communities, (community) => (community.key === "unknown" ? Infinity : -nodesPerCommunity[community.key])),
     [communities, nodesPerCommunity],
   );
+
+  const groupedCommunities = useMemo(() => {
+    return groupBy(sortedCommunities, 'healthzone');
+  }, [sortedCommunities]);
+
+  const healthZones = useMemo(() => {
+    return sortBy(Object.keys(groupedCommunities));
+  }, [groupedCommunities]);
+
+  const toggleHealthZone = (healthZone: string) => {
+    const communitiesInZone = groupedCommunities[healthZone];
+    const allChecked = communitiesInZone.every(community => filters.communities[community.key]);
+    const newState = !allChecked;
+    
+    const updatedCommunities = { ...filters.communities };
+    communitiesInZone.forEach(community => {
+      updatedCommunities[community.key] = newState;
+    });
+    
+    setCommunities(updatedCommunities);
+  };
+
+  const renderCommunity = (community: Community) => {
+    const nodesCount = nodesPerCommunity[community.key];
+    const visibleNodesCount = visibleNodesPerCommunity[community.key] || 0;
+    return (
+      <li
+        className="caption-row"
+        key={community.key}
+        title={`${nodesCount} page${nodesCount > 1 ? "s" : ""}${
+          visibleNodesCount !== nodesCount ? ` (only ${visibleNodesCount} visible)` : ""
+        }`}
+      >
+        <input
+          type="checkbox"
+          checked={filters.communities[community.key] || false}
+          onChange={() => toggleCommunity(community.key)}
+          id={`community-${community.key}`}
+        />
+        <label htmlFor={`community-${community.key}`}>
+          <span
+            className="circle"
+            style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/${community.image})` }}
+          />{" "}
+          <div className="node-label">
+            <span>{community.key}</span>
+            <div className="bar" style={{ width: (100 * nodesCount) / maxNodesPerCommunity + "%" }}>
+              <div
+                className="inside-bar"
+                style={{
+                  width: (100 * visibleNodesCount) / nodesCount + "%",
+                }}
+              />
+            </div>
+          </div>
+        </label>
+      </li>
+    );
+  };
 
   return (
     <Panel
@@ -69,45 +125,19 @@ const CommunitiesPanel: FC<{
           <AiOutlineCloseCircle /> Uncheck all
         </button>
       </p>
-      <ul>
-        {sortedCommunities.map((community: Community) => {
-          const nodesCount = nodesPerCommunity[community.key];
-          const visibleNodesCount = visibleNodesPerCommunity[community.key] || 0;
-          return (
-            <li
-              className="caption-row"
-              key={community.key}
-              title={`${nodesCount} page${nodesCount > 1 ? "s" : ""}${
-                visibleNodesCount !== nodesCount ? ` (only ${visibleNodesCount} visible)` : ""
-              }`}
-            >
-              <input
-                type="checkbox"
-                checked={filters.communities[community.key] || false}
-                onChange={() => toggleCommunity(community.key)}
-                id={`community-${community.key}`}
-              />
-              <label htmlFor={`community-${community.key}`}>
-                <span
-                  className="circle"
-                  style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/${community.image})` }}
-                />{" "}
-                <div className="node-label">
-                  <span>{community.key}</span>
-                  <div className="bar" style={{ width: (100 * nodesCount) / maxNodesPerCommunity + "%" }}>
-                    <div
-                      className="inside-bar"
-                      style={{
-                        width: (100 * visibleNodesCount) / nodesCount + "%",
-                      }}
-                    />
-                  </div>
-                </div>
-              </label>
-            </li>
-          );
-        })}
-      </ul>
+      {healthZones.map((healthZone) => (
+        <div key={healthZone}>
+          <h3>
+            {healthZone}
+            <button className="btn btn-small" onClick={() => toggleHealthZone(healthZone)}>
+              Toggle All
+            </button>
+          </h3>
+          <ul>
+            {groupedCommunities[healthZone].map(renderCommunity)}
+          </ul>
+        </div>
+      ))}
     </Panel>
   );
 };
